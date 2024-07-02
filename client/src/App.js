f8607bb06c1c0588e8826ffe36365d7a38172d55
Ritam Hudait <ritam.hudait@in.ey.com>
2024-07-02 10:48:26 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './components/home';
+import Header from './components/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
